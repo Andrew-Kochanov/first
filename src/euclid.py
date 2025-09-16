2aafa686e0e58ae62de0250b9210385cd08453bd
Andrew-Kochanov <andkochanov2019@gmail.com>
2025-09-17 00:11:11 +0300

    Правильный расширенный алгоритм Евклида

--- src/euclid.py
+++ src/euclid.py
@@ -1,8 +1,17 @@
 def gcd(a, b):
-    while a != b:
-        a, b = max(a, b), min(a, b)
-        a -= b
-    return (abs(a), abs(b))
+    x_1 = 1
+    y_1 = 0
+    x_2 = 0        ##коэфиценты для a и b
+    y_2 = 1
+    while b != 0:
+        x_1, x_2 = x_2, x_1 - (a // b) * x_2
+        y_1, y_2 = y_2, y_1 - (a // b) * y_2
+        a, b = b, a % b
+
+    return [a, x_1, y_1]            ##выводит список: нод, коэф а, коэф b
 
 
-print(gcd(81, 27))
+number_f = int(input('Введите первое число: '))
+number_s = int(input('Введите второе число: '))
+answer = gcd(number_f, number_s)
+print(f'{number_f} * {answer[1]} + {number_s} * {answer[2]} = {answer[0]}')
